1ec65cf786b03046b4f2caf927eb2cfd868552af
Akash066-creator <akash8423yadav@gmail.com>
2026-01-25 14:18:40 +0000

    data type done

---
+++ 01_basic/01_dataType.js
@@ -0,0 +1,23 @@
+"use strict" // treat all js code as newer version 
+// alert(3+3); // we are using nodejs, not browser
+let isLoggedIn=false;
+let name = "Akash";
+let age = 23;
+
+let state;
+let temprature = null;  
+
+
+// number => 2 to power 53
+// bigint
+// string => "single or double qute"
+// boolean => false/true
+// null => stand alone value
+// undefind => not assigned
+ // symble => unique 
+ 
+ console.log(typeof "hitesh") // string
+ console.log(typeof age) //number
+ console.log(typeof null) // object
+ console.log(typeof undefined) // undefined
+ 
