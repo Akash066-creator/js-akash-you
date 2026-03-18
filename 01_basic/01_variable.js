2ad74a73579d2213ca03d317530b0eb191766b3c
Akash066-creator <akash8423yadav@gmail.com>
2026-03-18 18:01:48 +0000

    variable done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+console.log("hello akash");
+
+const accountId = 10001;
+let accountEmail = "akash@gmail.com";
+var accountPassword = "12345678";
+let accountCity = "Allahabad";
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountCity);
+console.log(accountPassword);
+
+console.table([accountCity,accountEmail,accountId,accountPassword]);
